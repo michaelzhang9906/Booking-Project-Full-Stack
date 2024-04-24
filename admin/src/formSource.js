97e012a02cd6d1548623598a10b9f43610bed980
michaelzhang9906 <mzhang.pro.99@gmail.com>
2024-04-23 23:02:17 -0400

    admin without .git

---
+++ admin/src/formSource.js
@@ -0,0 +1,116 @@
+export const userInputs = [
+    {
+      id: "username",
+      label: "Username",
+      type: "text",
+      placeholder: "john_doe",
+    },
+    {
+      id: "email",
+      label: "Email",
+      type: "email",
+      placeholder: "John_Doe@gmail.com",
+    },
+    {
+      id: "phone",
+      label: "Phone",
+      type: "text",
+      placeholder: "+1 703 545 7876",
+    },
+    {
+      id: "password",
+      label: "Password",
+      type: "password",
+    },
+    {
+      id: "country",
+      label: "Country",
+      type: "country",
+      placeholder:"USA",
+    },
+    {
+      id: "city",
+      label: "City",
+      type: "text",
+      placeholder: "USA",
+    },
+  ];
+  
+  export const hotelInputs = [
+    {
+      id: "name",
+      label: "Name",
+      type: "text",
+      placeholder: "My Hotel",
+    },
+    {
+      id: "type",
+      label: "Type",
+      type: "text",
+      placeholder: "Hotel",
+    },
+    {
+      id: "city",
+      label: "City",
+      type: "text",
+      placeholder: "New York",
+    },
+    {
+      id: "address",
+      label: "Address",
+      type: "text",
+      placeholder: "Elton St, 216",
+    },
+    {
+      id: "distance",
+      label: "Distance from City Center",
+      type: "text",
+      placeholder: "500m",
+    },
+    {
+      id: "title",
+      label: "Title",
+      type: "text",
+      placeholder: "The best Hotel",
+    },
+    {
+      id: "desc",
+      label: "Discription",
+      type: "text",
+      placeholder: "description",
+    },
+    {
+      id: "cheapestPrice",
+      label: "Cheapest Price",
+      type: "text",
+      placeholder: "$100",
+    },
+  ];
+
+  export const roomInputs = [
+    {
+      id: "title",
+      label: "Title",
+      type: "text",
+      placeholder: "Twin Room",
+    },
+    {
+      id: "desc",
+      label: "Description",
+      type: "text",
+      placeholder: "King Size bed, 1 bathroom",
+    },
+    {
+      id: "price",
+      label: "Price",
+      type: "number",
+      placeholder: "100",
+    },
+    {
+      id: "maxPeople",
+      label: "Max People",
+      type: "number",
+      placeholder: "2",
+    },
+  ];
+  
